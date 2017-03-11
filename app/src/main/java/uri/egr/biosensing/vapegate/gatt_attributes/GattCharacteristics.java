8bcb02257962ebbe6ac1aa05aa93e58f6e1acdd8
mconstant4 <mconstant@my.uri.edu>
2017-03-11 12:26:14 -0500

    Initial Commit

---
+++ app/src/main/java/uri/egr/biosensing/vapegate/gatt_attributes/GattCharacteristics.java
@@ -0,0 +1,14 @@
+package uri.egr.biosensing.vapegate.gatt_attributes;
+
+import java.util.UUID;
+
+/**
+ * Created by mcons on 3/11/2017.
+ */
+
+public class GattCharacteristics {
+    public static final UUID TX_CHARACTERISTIC = UUID.fromString("713d0003-503e-4c75-ba94-3148f18d941e");
+    public static final UUID RX_CHARACTERISTIC = UUID.fromString("713d0002-503e-4c75-ba94-3148f18d941e");
+
+    public static final UUID WIOT_CHARACTERISTIC = UUID.fromString("713d0004-503e-4c75-ba94-3148f18d941e");
+}
---
+++ app/src/main/java/uri/egr/biosensing/vapegate/gatt_attributes/GattDescriptors.java
@@ -0,0 +1,11 @@
+package uri.egr.biosensing.vapegate.gatt_attributes;
+
+import java.util.UUID;
+
+/**
+ * Created by mcons on 3/11/2017.
+ */
+
+public class GattDescriptors {
+    public static final UUID NOTIFICATION_DESCRIPTOR = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+}
