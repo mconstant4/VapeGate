c2f20c1c258a2c30a87de0316f10f6d73695362b
mconstant4 <mconstant@my.uri.edu>
2017-03-12 10:11:19 -0400

    Added Watson

---
+++ app/src/main/java/uri/egr/biosensing/vapegate/services/DataLogService.java
@@ -0,0 +1,8 @@
+package uri.egr.biosensing.vapegate.services;
+
+/**
+ * Created by mcons on 3/11/2017.
+ */
+
+public class DataLogService {
+}
---
+++ app/src/main/java/uri/egr/biosensing/vapegate/utils/MyIoTCallbacks.java
@@ -0,0 +1,8 @@
+package uri.egr.biosensing.vapegate.utils;
+
+/**
+ * Created by mcons on 3/11/2017.
+ */
+
+public class MyIotCallbacks {
+}
