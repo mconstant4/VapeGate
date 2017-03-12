97d5301166538e81f2cef7782e59b97d25cf9376
mconstant4 <mconstant@my.uri.edu>
2017-03-11 19:09:48 -0500

    Added Settings

--- app/src/main/java/uri/egr/biosensing/vapegate/Settings.java
+++ app/src/main/java/uri/egr/biosensing/vapegate/Settings.java
@@ -1,10 +1,39 @@
 package uri.egr.biosensing.vapegate;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+import uri.egr.biosensing.vapegate.services.BandConnectionService;
+import uri.egr.biosensing.vapegate.services.VapeGateManagerService;
+
 /**
  * Created by nickp on 3/11/2017.
  */
 
-public class Settings extends First
-{
+public class Settings extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private Context mContext;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+        mContext = this;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case "vapegate_enable":
+                if (sharedPreferences.getBoolean("vapegate_enable", false)) {
+                    BandConnectionService.connect(mContext);
+                    VapeGateManagerService.connect(mContext);
+                } else {
+                    BandConnectionService.disconnect(mContext);
+                    VapeGateManagerService.disconnect(mContext);
+                }
+                break;
+        }
+    }
 }
